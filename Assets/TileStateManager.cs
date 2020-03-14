ec6de73a2cf49f974773e87ede20b1947fbc30e9
swanickj <swanickj@tipmedia.com>
2020-03-14 19:56:56 -0400

    first commit!!!!!!!!!!!

---
+++ Assets/TileStateManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileStateManager : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class GameTile {
+    public string name = "tile";
+    public GameTile(string name, float sunlight, float water) {
+
+    }
+}
