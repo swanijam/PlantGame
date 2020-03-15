1158e892ee1f24e2b13b574f5c412c1db6c99bc6
swanickj <swanickj@tipmedia.com>
2020-03-14 22:04:01 -0400

    basic tile management and rendering

--- Assets/TileStateManager.cs
+++ Assets/TileStateManager.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class TileStateManager : MonoBehaviour
 {
-    public 
+    public Vector2Int dimensions;
+    public GameTile[,] tiles;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Initialize();   
+    }
+    void Initialize() {
+        tiles = new GameTile[dimensions.x, dimensions.y];
+        for (int x = 0; x < dimensions.x; x++ ) {
+            for (int y = 0; y < dimensions.y; y++ ) {
+                tiles[x,y] = new GameTile();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AddSunlight(int x, int y, int amt) {
+        tiles[x,y].sunlightLevel += amt;
+    }
+    public void AddWater(int x, int y, int amt) {
+        tiles[x,y].waterLevel += amt;
     }
 }
 
 [System.Serializable]
 public class GameTile {
     public string name = "tile";
-    public GameTile(string name, float sunlight, float water) {
-
+    public int sunlightLevel;
+    public int waterLevel;
+    public GameTile(string name ="", int sunlight =1, int water =1) {
+        this.name = name;
+        this.sunlightLevel = sunlight;
+        this.waterLevel = water;
     }
 }
---
+++ Assets/TilerRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilerRenderer : MonoBehaviour
+{
+    public TileStateManager state;
+    public Camera mainCamera;
+    public Transform origin;
+    public float tileWidth;
+    public float heightOffset;
+    public Vector2 boxdimensions;
+    // Update is called once per frame
+    private void Awake()
+    {
+    }
+    void OnGUI() {
+        RenderTilesWithGUI();
+    }
+    private void RenderTilesWithGUI() {
+        Vector3 tileOffset = new Vector3(tileWidth, 0f, tileWidth);
+        // buffers
+        Rect box;string statusString;Vector3 labelPositionW;Vector2 labelPositionS;
+        // draw status box at center of each tile on screen
+        for (int x = 0; x < state.dimensions.x; x++ ) {
+            for (int y = 0; y < state.dimensions.y; y++ ) {
+               labelPositionW = origin.position + Vector3.Scale(tileOffset, new Vector3(x, 0, y)) + heightOffset * Vector3.up + new Vector3(tileWidth/2f, 0f, tileWidth/2f);
+               labelPositionS = mainCamera.WorldToScreenPoint(labelPositionW);
+               labelPositionS.y = Screen.height - labelPositionS.y; // flip the y coordinate to convert from camera screen space to GUI space.
+               box = new Rect(labelPositionS.x, labelPositionS.y, boxdimensions.x, boxdimensions.y);
+               Rect box2 = box;
+               box2.x -= 3; box2.y -= 3;
+               box2.height = 6; box2.width = 6;
+               statusString = "sun:"+state.tiles[x, y].sunlightLevel +"\nwtr:"+state.tiles[x, y].waterLevel; 
+               GUI.Box(box, statusString);
+               GUI.DrawTexture(box2, Texture2D.whiteTexture, ScaleMode.StretchToFill, false, 1f, Color.yellow, 0f, 0f);
+            }
+        }
+    }
+}
